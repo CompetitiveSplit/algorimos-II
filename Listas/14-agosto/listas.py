356817d7dae8f42fa803c91f736732df37f5ab25
bl1nk <117480597+CompetitiveSplit@users.noreply.github.com>
2024-08-14 20:19:17 +0000

    Clase 1

---
+++ Listas/14-agosto/listas.py
@@ -0,0 +1,43 @@
+""" 
+    Características 
+    - Estructura de datos
+    - Unidimensional
+    - Dinámica (No requiere un valor previo, puede adaptarse al uso)
+    - Flexible
+    - Eficiente en uso de memoria
+    - Diseño para árboles y grafos
+"""
+# Clase nodo
+class Nodo:
+    def __init__(self, dato) -> None:
+        self.dato = dato 
+        self.siguiente = None
+
+    # metodo STR
+    def __str__(self) -> str:
+        return str(self.dato)
+
+# Clase lista
+class Lista:
+    def __init__(self) -> None:
+        self.inicial = None
+
+    # Agregar elemento al inicio de la lista
+    def adicionarAlInicio(self, dato):
+        # Evaluar si la lista esta vacia
+        if self.inicial == None: # La lista está vacia 
+            self.inicial = Nodo(dato)
+        else: 
+            # Si la lista tiene, al menos, un elemento
+            nodo_nuevo = Nodo(dato)     # Se crea una variable para almacenar el nuevo objeto (Un nuevo nodo)
+            nodo_nuevo.siguiente = self.inicial     
+            self.inicial = nodo_nuevo
+
+    # Recorrer la lista
+    def __str__(self) -> str:
+        recorrido = " "
+        nodo_actual = self.inicial
+        while nodo_actual != None: 
+            recorrido += str(nodo_actual) + " -> "
+            nodo_actual = nodo_actual.siguiente
+        return recorrido
---
+++ Listas/14-agosto/prueba_obj.py
@@ -0,0 +1,22 @@
+from listas import *
+
+# # Creación de nodos
+# nodo_01 = (1)
+# nodo_02 = (3.5)
+# nodo_03 = "Hola"
+
+# # Prueba de los nodos
+# print("Nodo 01: ", nodo_01)
+# print("Nodo 02: ", nodo_02)
+# print("Nodo 03: ", nodo_03)
+
+# Prueba de lista
+lista_01 = Lista()      # Creación de la lista
+lista_01.adicionarAlInicio(1)       # Adicionar elementos a lista
+lista_01.adicionarAlInicio(3.5)
+lista_01.adicionarAlInicio("Hola")
+
+print("Lista: ", lista_01)      # Imprimir la lista
+
+
+# NOTA: Adelantar adicionar al final.
