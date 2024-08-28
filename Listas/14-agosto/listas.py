38fc7ff60d45e0d2f0dea3dd860a48bb894a1d57
jazzy <narvaez0526@gmail>
2024-08-27 19:51:47 -0500

    v0.1 del primer taller de  diseño

--- Listas/14-agosto/listas.py
+++ Listas/14-agosto/listas.py
@@ -30,28 +30,6 @@ class Lista:
         # Si la lista tiene datos
         self.inicial = self.inicial.siguiente
         return True
-    
-    # Eliminar por información (Si el dato existe)
-    def eliminarPorInfo(self, dato):
-    # Si la lista está vacía 
-        if self.inicial is None:
-            return False
-        # El dato se encuentra en la cabecera
-        if self.inicial.dato == dato:
-            self.eliminarAlInicio()
-            return True
-        # Buscar dato en la lista
-        nodo_previo = None
-        nodo_actual = self.inicial
-        while nodo_actual is not None and nodo_actual.dato != dato:
-            nodo_previo = nodo_actual
-            nodo_actual = nodo_actual.siguiente
-        # Dato no encontrado
-        if nodo_actual is None:
-            return False
-        # Dato encontrado
-        nodo_previo.siguiente = nodo_actual.siguiente
-        return True
 
     # Inidicar si un dato esta en la lista
     def buscar(self, dato):
@@ -66,6 +44,28 @@ class Lista:
             nodo_actual = nodo_actual.siguiente
         return False # No encontrado
 
+    # Eliminar por información (Si el dato existe)
+    def eliminarPorInfo(self, dato):
+    # Si la lista está vacía 
+        if self.inicial == None:
+            return False
+        # El dato se encuentra en la cabecera
+        if self.inicial.dato == dato:
+            self.eliminarAlInicio()
+            return True
+        # Buscar dato en la lista
+        nodo_previo = None
+        nodo_actual = self.inicial
+        while nodo_actual != None and nodo_actual.dato != dato:
+            nodo_previo = nodo_actual
+            nodo_actual = nodo_actual.siguiente
+        # Dato no encontrado
+        if nodo_actual == None:
+            return False
+        # Dato encontrado
+        nodo_previo.siguiente = nodo_actual.siguiente
+        return True
+    
     # Agregar elemento al inicio de la lista
     def adicionarAlInicio(self, dato):
         # Evaluar si la lista esta vacia
@@ -86,3 +86,106 @@ class Lista:
             nodo_actual = nodo_actual.siguiente
         recorrido += "NULL" 
         return recorrido
+    
+
+# TALLER DE DISENO 1
+
+    # Agregar al final de la lista
+    def agregarAlFinal(self, dato):
+        nodo_actual = self.inicial
+        nodo_nuevo = Nodo(dato)     # Nodo nuevo
+        # Evaluar si la lista esta vacia
+        if self.inicial == None:
+            self.inicial = nodo_nuevo
+        else:
+            nodo_actual = self.inicial
+        # Recorrer la lista
+            while nodo_actual.siguiente != None:
+                nodo_actual = nodo_actual.siguiente
+            nodo_actual.siguiente = nodo_nuevo     # enlazar el actual al siguiente
+            nodo_nuevo.siguiente = None     # Apuntar el ultimo a NULL, en este caso None
+
+    # Indicar la cantidad de elementos de la lista
+    def contarElementos(self):
+        nNodos= 0       # Variable contadora donde se almacena el numero de nodos
+        nodo_actual = self.inicial
+        if nodo_actual == None:
+            return 0
+        while nodo_actual != None:
+            nodo_actual = nodo_actual.siguiente
+            nNodos = nNodos + 1
+        return nNodos
+    
+    # Eliminar el ultimo elemento de la lista
+    def eliminarUltimo(self):
+        nodo_actual = self.inicial
+        nodo_previo = None
+        if nodo_actual == None:
+            return False
+        else:
+            while nodo_actual.siguiente != None:
+                nodo_previo = nodo_actual
+                nodo_actual = nodo_actual.siguiente
+            nodo_previo.siguiente = None
+        
+    # Indicar el numero de apariciones de un dato en la lista
+    def contarApariciones(self, dato):
+        nodo_actual = self.inicial
+        nApariciones = 0
+        if self.inicial == None:
+            return False    # Retorna false si la lista esta vacia
+        while nodo_actual != None:
+            if nodo_actual.dato == dato:
+                nApariciones = nApariciones + 1
+            nodo_actual = nodo_actual.siguiente     # Recorrido de la lista
+        return nApariciones
+
+    def indicarElemento(self, pos):
+        cont = 0
+        nodo_actual = self.inicial
+        # Verificar si la lista esta vacia
+        if nodo_actual == None:
+            return False
+        # Recorrer la lista hasta la posicion deseada
+        while nodo_actual != None and cont < pos:
+            nodo_actual = nodo_actual.siguiente
+            cont = cont + 1
+        # Verificar si se alcanzo una posición valida
+        if nodo_actual == None:
+            return False  # Posicion fuera del rango
+        else:
+            return nodo_actual.dato
+        
+    def eliminarPosIndicada(self, pos):
+        cont = 0
+        nodo_actual = self.inicial
+        nodo_previo = None
+        if nodo_actual == None:
+            return False
+        while nodo_actual != None and cont < pos:
+            nodo_previo = nodo_actual
+            nodo_actual = nodo_actual.siguiente
+            cont = cont + 1
+        if nodo_actual == None:
+            return False
+        else:
+            nodo_previo.siguiente = nodo_actual.siguiente   # "salto"
+            return True 
+        
+    # Indicar las posiciones en las que aparece un dato en la lista
+    def contarPosiciones(self, dato):
+        cont = 0 
+        posiciones = []
+        nodo_actual = self.inicial
+        if nodo_actual == None:
+            return False
+        while nodo_actual != None:
+            if nodo_actual.dato == dato:
+                posiciones.append(cont)
+            nodo_actual = nodo_actual.siguiente
+            cont = cont + 1
+        return posiciones
+            
+
+        
+                                 
--- Listas/14-agosto/prueba_obj.py
+++ Listas/14-agosto/prueba_obj.py
@@ -12,14 +12,34 @@ from listas import *
 
 # Prueba de lista
 lista_01 = Lista()      # Creación de la lista
+
 lista_01.adicionarAlInicio(1)       # Adicionar elementos a lista
 lista_01.adicionarAlInicio(3.5)
-lista_01.adicionarAlInicio("Hola")
+# lista_01.adicionarAlInicio("Hola")
 lista_01.adicionarAlInicio(10) 
 lista_01.adicionarAlInicio(6) 
 lista_01.adicionarAlInicio(4) 
+lista_01.agregarAlFinal(4)
+lista_01.agregarAlFinal(4)
+lista_01.agregarAlFinal("text")
+# Prueba agregar al final de la lista
+# lista_01.agregarAlFinal("lol")
+# lista_01.eliminarUltimo()
+posicion = "x"
+print(f"elemento de la posicion {posicion}: {lista_01.indicarElemento(4)}")
+# Prueba contar elementos de la lista
+# lista_01.eliminarPosIndicada(4)
+print(f"el numero {posicion}, aparece en las posiciones: {lista_01.contarPosiciones(4)}")
+print(lista_01.contarElementos(), "elementos en la lista")
+
+# Prueba contar apariciones de un elemento
+buscar = 4
+print(f"el numero {buscar} aparece: {lista_01.contarApariciones(4)} veces")
 
 print("Lista: ", lista_01)      # Imprimir la lista
+
+
+
 
 # Prueba eliminar al incio
 # print(lista_01.eliminarAlInicio())  # retorna true: El metodo está funcionando
@@ -27,7 +47,7 @@ print("Lista: ", lista_01)      # Imprimir la lista
 # print("Lista: ", lista_01)
 
 # Prueba eliminar por info
-print(lista_01.eliminarPorInfo("Hola"))
-print("Lista: ", lista_01)
+# print(lista_01.eliminarPorInfo("Hola"))
+# print("Lista: ", lista_01)
 
 # NOTA: Adelantar adicionar al final.
